750106a06e1b3b7be347cf503e2596fd522b2114
Najib Mestaoui <nmestaoui@fujikura-automotive.com>
2017-04-13 10:25:41 +0100

    still implementing process

--- src/process.rs
+++ src/process.rs
@@ -1,11 +1,14 @@
 extern crate winapi;
 extern crate kernel32;
 
+use std::ffi::OsString;
 use std::io::{Error};
 use std::mem;
 use std::ptr;
 use std::os::windows::raw::HANDLE;
+use std::os::windows::ffi::OsStringExt;
 use std::string::String;
+use std::vec:: { Vec };
 
 use self::winapi::shlobj::INVALID_HANDLE_VALUE;
 use self::winapi::tlhelp32::{PROCESSENTRY32W
@@ -18,6 +21,7 @@ use self::winapi::tlhelp32::{TH32CS_SNAPPROCESS
                              , TH32CS_SNAPMODULE32
                              , TH32CS_SNAPMODULE};
 
+const INITIAL_PROCESS_VECTOR_SIZE: u32 = 20;
 pub struct Process {
   win_process_entry: PROCESSENTRY32W,
   main_module: MODULEENTRY32W,
@@ -32,8 +36,43 @@ impl Process {
     }
   }
 
-  pub fn get_processes() -> Vec<Process> {
 
+  /// Get a list of currently running process in the system as `Vec`
+  /// # Example
+  /// ```
+  /// use process::Process;
+  ///
+  /// let processes = Process::get_processes();
+  /// for process in processes {
+  ///   println!("{}: {}", process.get_process_id(), process.get_process_name().unwrap();
+  /// }
+  /// ```
+  pub fn get_processes() -> Result<Vec<Process>, String> {
+    let mut vec: Vec<Process> = Vec::new();
+    let (handle, first_process) = try!(Process::get_first());
+    vec.push(first_process);
+
+    while let Ok(next_process) = Process::get_next(handle) {
+      vec.push(next_process);
+    }
+    //close the handle at OS side
+    Process::close_handle(handle);
+    return Ok(vec);
+  }
+  fn get_next(snapshot_handle: HANDLE) -> Result<Process, String> {
+    let mut win_process_entry = Process::create_new_process_entry();
+    {
+      let mut_win_process_entry = &mut win_process_entry;
+      let return_state = unsafe { kernel32::Process32NextW(snapshot_handle, mut_win_process_entry)};
+      if return_state == FALSE {
+        return Err(format!("Could not retrieve next process entry: {:?}"
+                           , Error::last_os_error()));
+      }
+    }
+
+    let module = try!(Process::get_main_module(win_process_entry.th32ProcessID));
+
+    Ok(Process::new(win_process_entry, module))
   }
   fn get_first() -> Result<(HANDLE, Process), String> {
     let snapshot_handle = unsafe { kernel32::CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0) };
@@ -42,11 +81,13 @@ impl Process {
                          , Error::last_os_error()));
     }
     let mut win_process_entry = Process::create_new_process_entry();
-    let a = &mut win_process_entry;
-    let return_state = unsafe { kernel32::Process32FirstW(snapshot_handle, a) };
-    if return_state == FALSE {
-      return Err(format!("Could not retrieve 1st process entry: {:?}"
-                         , Error::last_os_error()));
+    {
+      let a = &mut win_process_entry;
+      let return_state = unsafe { kernel32::Process32FirstW(snapshot_handle, a) };
+      if return_state == FALSE {
+        return Err(format!("Could not retrieve 1st process entry: {:?}"
+                           , Error::last_os_error()));
+      }
     }
     let process_id = win_process_entry.th32ProcessID;
     let module = try!(Process::get_main_module(process_id));
@@ -68,15 +109,17 @@ impl Process {
     }
 
     let mut module = Process::create_new_module();
-    let mdl = &mut module;
-    let return_state = unsafe {
-      kernel32
-      ::Module32FirstW(snapshot_handle, mdl)
-    };
+    {
+      let mdl = &mut module;
+      let return_state = unsafe {
+        kernel32
+        ::Module32FirstW(snapshot_handle, mdl)
+      };
 
-    if return_state == FALSE {
-      return Err(format!("Could not get main module: {:?}"
-                         , Error::last_os_error()));
+      if return_state == FALSE {
+        return Err(format!("Could not get main module: {:?}"
+                           , Error::last_os_error()));
+      }
     }
 
     Ok(module)
@@ -111,5 +154,26 @@ impl Process {
   }
   fn close_handle(handle: HANDLE) {
     unsafe { kernel32::CloseHandle(handle)};
+  }
+  pub fn get_process_id(&self) -> u32 {
+    self.win_process_entry.th32ProcessID
+  }
+  pub fn get_thread_count(&self) -> u32 {
+    self.win_process_entry.cntThreads
+  }
+  pub fn get_parent_process_id(&self) -> u32 {
+    self.win_process_entry.th32ParentProcessID
+  }
+  pub fn get_base_priority(&self) -> i32 {
+    self.win_process_entry.pcPriClassBase
+  }
+  pub fn get_process_name(&self) -> Result<String, String> {
+    let name = self.win_process_entry.szExeFile;
+    let exe_name : OsString = OsString::from_wide(name.iter()
+        .position(|c| *c == 0)
+        .map(|i| &name[..i])
+        .unwrap_or(&name));
+    exe_name.into_string()
+      .or(Err("Could not retrieve process name".to_string()))
   }
 }
